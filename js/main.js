02d36794ba10103fb26c43e2d22946d7c6a6a68f
Melih Doksanbir <melih.doksanbir@remmicom.be>
2023-04-03 19:06:54 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-const imageFolder = 'resimler/';
-fetch(imageFolder)
-  .then(response => response.text())
-  .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const imageFiles = Array.from(doc.querySelectorAll('a[href$=".jpg"],a[href$=".png"],a[href$=".jpeg"],a[href$=".gif"]'))
-      .map(link => link.getAttribute('href'));
-    // Call the function to create the carousel with the list of image files
-    createCarousel(imageFiles);
+const carousel = document.getElementById('carousel');
+
+// Get a list of image file names
+fetch('images.json')
+  .then(response => response.json())
+  .then(data => {
+    // Create a div for each image and append it to the carousel
+    data.forEach(filename => {
+      const div = document.createElement('div');
+      div.innerHTML = `<img src="resimler/${filename}">`;
+      carousel.appendChild(div);
+    });
+
+    // Use Intersection Observer API to lazy load images
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target.querySelector('img');
+          img.src = img.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    // Observe each image in the carousel
+    const images = carousel.querySelectorAll('img');
+    images.forEach(img => {
+      img.dataset.src = img.src;
+      img.src = '';
+      observer.observe(img.parentNode);
+    });
   });
