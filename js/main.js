371feea18ce46edc78333993880da92dbb8e1695
Melih Doksanbir <melih.doksanbir@remmicom.be>
2023-04-03 19:29:27 +0200

    test2

--- js/main.js
+++ js/main.js
@@ -1,32 +1,44 @@
-const carousel = document.getElementById('carousel');
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
 
-// Get a list of image file names
-fetch('images.json')
-  .then(response => response.json())
-  .then(data => {
-    // Create a div for each image and append it to the carousel
-    data.forEach(filename => {
-      const div = document.createElement('div');
-      div.innerHTML = `<img src="resimler/${filename}">`;
-      carousel.appendChild(div);
-    });
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if(current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
 
-    // Use Intersection Observer API to lazy load images
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const img = entry.target.querySelector('img');
-          img.src = img.dataset.src;
-          observer.unobserve(entry.target);
-        }
-      });
-    });
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if(current.previousElementSibling) {
+    // Add current to next sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
 
-    // Observe each image in the carousel
-    const images = carousel.querySelectorAll('img');
-    images.forEach(img => {
-      img.dataset.src = img.src;
-      img.src = '';
-      observer.observe(img.parentNode);
-    });
-  });
+// Auto slide
+if(auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
